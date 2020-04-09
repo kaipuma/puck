53030878c8c5b1a617176e01d483b9f4a64e5e31
kaipuma <andrew@kaipuma.com>
2020-04-09 01:16:30 -0230

    add the emoji listed in the emoji.json config file to each server on startup, if that named emoji isn't already there

--- bot.py
+++ bot.py
@@ -14,6 +14,26 @@ puck.help_command.cog = puck.cogs["Other"]
 async def quit(ctx):
 	await puck.close()
 
+@puck.event
+async def on_ready():
+	with open("./configs/emoji.json", "r") as file:
+		emojis = json.load(file)
+
+	for guild in puck.guilds:
+		for path, name in emojis.items():
+			if any(e.name == name for e in guild.emojis): continue
+			with open(path, "rb") as file:
+				try:
+					print("Uploading emoji:", name)
+					await guild.create_custom_emoji(
+						name=name,
+						image=file.read(),
+						reason="required emoji"
+					)
+				except BaseException as e:
+					print(e)
+	print("Done updating emoji")
+
 if __name__ == '__main__':
 	with open("configs/data.json", "r") as file:
 		puck.data = json.load(file)
